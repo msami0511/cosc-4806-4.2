12c052ced75fd84d8e2fc9912b82bbe5aca05d35
msami0511 <mohamashaikh@algomau.ca>
2025-07-05 02:58:32 +0000

    Create method added to reminders

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -1,9 +1,34 @@
 <?php
+class Reminders extends Controller {
 
-class Reminders extends Controller{
-    public function index(){
-      $R=$this->model('Reminder');
-      $list_of_reminders=$R->get_all_reminders();
-        $this->view('reminders/index',['reminders'=>$list_of_reminders]);
+    public function index() {
+        if (!isset($_SESSION['auth']) || $_SESSION['auth'] != 1) {
+            header('Location: /login');
+            exit;
+        }
+
+        $reminderModel = $this->model('Reminder');
+        $reminders = $reminderModel->get_all_reminders();
+        $this->view('reminders/index', ['reminders' => $reminders]);
     }
-}
+  public function create() {
+      if (!isset($_SESSION['auth']) || $_SESSION['auth'] != 1) {
+          header('Location: /login');
+          exit;
+      }
+
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+          $subject = trim($_POST['subject']);
+          $userId = $_SESSION['user_id'];
+
+          $reminderModel = $this->model('Reminder');
+          $reminderModel->create_reminder($userId, $subject);
+
+
+      } else {
+          $this->view('reminders/create');
+      }
+  }
+
+
+  
