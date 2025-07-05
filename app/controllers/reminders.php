28ebf857e2facf18d50cd228d855b8a0f3e9bca7
msami0511 <mohamashaikh@algomau.ca>
2025-07-05 02:55:43 +0000

    Remainder page modified to connect to view

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -2,6 +2,8 @@
 
 class Reminders extends Controller{
     public function index(){
-        $this->view('reminders/index');
+      $R=$this->model('Reminder');
+      $list_of_reminders=$R->get_all_reminders();
+        $this->view('reminders/index',['reminders'=>$list_of_reminders]);
     }
 }
